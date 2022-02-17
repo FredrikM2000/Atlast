703f284cbcec85a01f17c3b7ecf586502409c238
FredrikM2000 <70491657+FredrikM2000@users.noreply.github.com>
2022-02-17 11:00:42 +0100

    Game

---
+++ Source/Atlast/AtlastGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AtlastGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ATLAST_API AAtlastGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
