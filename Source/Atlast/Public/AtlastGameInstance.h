dbf3597feba8f4aaf37dcb0e4a358e979e56d487
whiteNgreen <33910273+whiteNgreen@users.noreply.github.com>
2022-03-13 13:25:16 +0100

    Simple setup
    lagt til gameinstance og en enkel widget blueprint for main menu

---
+++ Source/Atlast/Public/AtlastGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "AtlastGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ATLAST_API UAtlastGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
